f9e9d99ed59292605cdd42900f94f2b15445d885
antoHero <veeqanto@gmail.com>
2020-05-31 20:26:13 +0100

    Updates

---
+++ precache-manifest.e14a487d5469318e00ec8c3692ca2710.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c39527c885ed4d04995aaf52d09ea5b0",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "7e9c40d7b0a1dddc0b79",
+    "url": "/monsters/static/css/main.31ae3a4f.chunk.css"
+  },
+  {
+    "revision": "2fcaebef661b57072565",
+    "url": "/monsters/static/js/2.5c2670a9.chunk.js"
+  },
+  {
+    "revision": "7e9c40d7b0a1dddc0b79",
+    "url": "/monsters/static/js/main.48a9878f.chunk.js"
+  },
+  {
+    "revision": "a780c5024727f4e9d1d4",
+    "url": "/monsters/static/js/runtime-main.4488f325.js"
+  }
+]);
